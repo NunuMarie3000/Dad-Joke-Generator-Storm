5815cc39a941c9a6d01948a757dc72e533c82607
Storm OBryant <vmarie1997@gmail.com>
2022-06-28 16:34:52 -0500

    added 33 joke array as backup for when api subscription jokes run out every day

--- app.js
+++ app.js
@@ -38,8 +38,30 @@ async function generateNewJoke(){
 
 }
 generateNewJoke();
-//add event listener to button
-anotherButton.addEventListener('click', generateNewJoke);
+let jokeArray = [];
+jokeArray = ["What do you call two monkeys who share an Amazon account? \n Prime mates.", "I told my wife she needs to start embracing her mistakes. \n So she hugged me.", "Who is the most lonely billionaire? \n Alone musk.", "I asked 10 people what LGTBQ standed for… \n Couldn’t get a straight answer!", "My wife and I let astrology get between us. \n It Taurus apart.", "Why can’t you send a duck to space? \n Because the bill would be astronomical.", "I once got fired from a canned juice factory. \n Apparently I couldn’t concentrate.", "Why did Karen press Ctrl+Shift+Delete? \n Because she wanted to see the task manager.", "I’m really excited for the amateur autopsy club I just joined. \n Tuesday is open Mike night!", "I can’t find my ‘Gone in 60 seconds’ DVD. \n It was here a minute ago.", "How was Rome split in two? \n With a pair of Ceasars.", "What kind of doctor is Dr. Pepper? \n A fizzician.", "Why didn’t the melons get married? \n Because they cantaloupe.", "Two guys walked into a bar. \n The third guy ducked.", "My son got angry when I told him 'Sky is the limit for you'. \n He wants to be an astronaut.", "What happens when it rains cats and dogs? \n You have to be careful not to step in a poodle.", "Did you know that bees are actually allergic to pollen? \n They break out in hives.", "I had a great joke about COVID… \n but I don’t wanna spread it around.", "How did the pirate get his ship so cheap? \n It was on sail..", "What do you call a laughing jar of mayonnaise? \n LMAYO.", "Did you hear about the cheese that’s been working out? \n Dude’s shredded", "What do you need to make a small fortune on Wall Street\? \n A large fortune.", "How do you follow Will Smith in the snow? \n You follow the fresh prints.", "What did Adam say to his GF on the 24th of December\? \n It’s Christmas Eve.", "My boss asked me why I only get sick on work days. \n I said it must be my weekend immune system.", "How does a non-binary samurai kill people? \n They/Them.", 'Friend: “Bro, can you pass me that pamphlet?” \n Me: “Brochure”', "Whoever stole my depression medication: \n I hope you’re happy.", "Why does Sherlock Holmes love Mexican restaurants? \n They give him good case ideas.", "Why can’t a leopard hide? Because he’s always spotted.", "The world tongue-twister champion just got arrested. I hear they’re gonna give him a really tough sentence.", "What do you call a zombie who cooks stir fries? Dead man wok-ing", "Which cat is the least loyal? A cheetah.",];
+function generateJoke(){
+    //declare variable to be random index #
+    let randomJoke = Math.floor(Math.random() * jokeArray.length);
+    //will return a random item from jokeArray
+    return jokeArray[randomJoke];
+}
+let jokeEl = document.createElement('h2');
+function printJoke(){
+    //print the joke out as the text content of the h2
+    jokeEl.textContent = generateJoke();
+    //add h2 to the html div
+    printJokeHere.append(jokeEl);
+}
+printJoke();
+//when the button is clicked, another random joke will be generated
+anotherButton.addEventListener('click', function(){
+    generateNewJoke();
+    generateJoke();
+    printJoke();
+});
+// //add event listener to button
+// anotherButton.addEventListener('click', generateNewJoke);
 
 
 
